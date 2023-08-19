de476bdb05f584bc0a2538cb3db8d73dd12c650b
onlinekkabchi <onlinekkabchi@gmail.com>
2023-08-19 17:06:30 +0900

    refactor: css 파일 분리, threejs 패키지 다운로드

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 import express from "express";
 import http from "http";
-import path from "path";
 import { Server } from "socket.io";
+import dotenv from "dotenv";
+import sql from "mssql";
+
+dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+const config = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_NAME,
+  options: {
+    encrypt: true,
+  },
+};
+const users = {};
 
 app.use(express.static("public"));
 
@@ -14,29 +27,38 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  // console.log("a user connected");
-  // socket.on("disconnect", () => {
-  //   console.log("user disconnected");
-  // });
+  socket.on("new user", async (username) => {
+    // Insert the new user into the database
+    try {
+      const pool = await sql.connect(config);
+      await pool
+        .request()
+        .query(`INSERT INTO Users (Username) VALUES ('${username}')`);
+    } catch (err) {
+      console.error("Error inserting user into database: ", err);
+    }
+
+    users[socket.id] = username;
+    io.emit("user joined", username);
+  });
+
   socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
+    io.emit("chat message", { user: users[socket.id], message: msg });
+  });
+
+  socket.on("disconnect", () => {
+    if (users[socket.id]) {
+      const username = users[socket.id];
+      io.emit("user left", username);
+      delete users[socket.id];
+    }
   });
 });
 
-io.emit("some event", {
-  someProperty: "some value",
-  otherProperty: "other value",
-});
-
-// io.on("connection", (socket) => {
-//   socket.broadcast.emit("hi");
+// io.emit("some event", {
+//   someProperty: "some value",
+//   otherProperty: "other value",
 // });
-
-io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
-  });
-});
 
 server.listen(3000, () => {
   console.log("listening on 3000");
--- public/main.js
+++ public/main.js
@@ -1,19 +1,34 @@
-var socket = io();
+document.addEventListener("DOMContentLoaded", function () {
+  var socket = io();
+  var form = document.getElementById("form");
+  var input = document.getElementById("input");
+  var messages = document.getElementById("messages");
 
-var form = document.getElementById("form");
-var input = document.getElementById("input");
-
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  if (input.value) {
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
     socket.emit("chat message", input.value);
     input.value = "";
-  }
-});
+  });
 
-socket.on("chat message", function (msg) {
-  var item = document.createElement("li");
-  item.textContent = msg;
-  messages.appendChild(item);
-  window.scrollTo(0, document.body.scrollHeight);
+  socket.on("chat message", function (msg) {
+    const item = document.createElement("li");
+    item.textContent = msg.user + ": " + msg.message;
+    messages.appendChild(item);
+    // window.scrollTo(0, document.body.scrollHeight);
+  });
+
+  socket.on("user joined", function (username) {
+    const li = document.createElement("li");
+    li.textContent = username + " joined the chat";
+    messages.appendChild(li);
+  });
+
+  socket.on("user left", function (username) {
+    const li = document.createElement("li");
+    li.textContent = username + " left the chat";
+    messages.appendChild(li);
+  });
+
+  const username = prompt("Enter your username: ");
+  socket.emit("new user ", username);
 });
