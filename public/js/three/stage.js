765ce37b86f007d37144db0f53a3a6f812e28b62
onlinekkabchi <onlinekkabchi@gmail.com>
2023-08-26 19:46:13 +0900

    feat: 완성모델 업로드 테스트

---
+++ public/js/three/stage.js
@@ -0,0 +1,59 @@
+// import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+
+const loader = new GLTFLoader();
+
+function stage(scene) {
+  // 무대 원본
+  loader.load(
+    "./static/model/stage.glb",
+    function (gltf) {
+      const model = gltf.scene;
+      model.position.set(1, 0, 0);
+      model.scale.set(1, 1, 1);
+      scene.add(model);
+    },
+    undefined,
+    function (err) {
+      console.error(err);
+    }
+  );
+}
+
+function stageBaked(scene) {
+  // 무대 베이킹본
+  loader.load(
+    "./static/model/stage2.glb",
+    function (gltf) {
+      const model = gltf.scene;
+      model.position.set(1, 0, 0);
+      model.scale.set(1, 1, 1);
+      scene.add(model);
+    },
+    undefined,
+    function (err) {
+      console.error(err);
+    }
+  );
+}
+
+// // 탱크
+// loader.load(
+//   "./static/model/Tiger_I1.glb",
+//   function (gltf) {
+//     const model = gltf.scene;
+//     model.position.set(0, 0, 0);
+//     model.scale.set(1, 1, 1);
+//     scene.add(model);
+
+//     // const animations = gltf.animations;
+//     // let mixer = new THREE.AnimationMixer(model);
+//     // mixer.clipAction(animations[0]).play();
+//   },
+//   undefined,
+//   function (err) {
+//     console.error(err);
+//   }
+// );
+
+export { stage, stageBaked };
