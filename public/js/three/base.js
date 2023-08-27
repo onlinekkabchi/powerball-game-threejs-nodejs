209270dbc35280a712ec153a5d90dcad09e1a28a
onlinekkabchi <onlinekkabchi@gmail.com>
2023-08-27 18:58:32 +0900

    test: 어두운버전 라이트닝 효과 없어짐

--- public/js/three/base.js
+++ public/js/three/base.js
@@ -40,8 +40,8 @@ document.body.appendChild(renderer.domElement);
 scene.add(cube1, cube2);
 
 // 무대 원본, 베이킹본
-stage(scene);
-// stageBaked(scene);
+// stage(scene);
+stageBaked(scene);
 
 // 달걀
 egg(scene);
@@ -49,6 +49,7 @@ egg(scene);
 // 빛!
 const light = new THREE.DirectionalLight(0xd5deff);
 light.position.set(400, 250, 500);
+scene.add(light);
 
 const bulbGeometry = new THREE.SphereGeometry(0.02, 16, 8);
 const bulbLight = new THREE.PointLight(0xffee88, 1, 100, 2);
@@ -60,17 +61,10 @@ const bulbMat = new THREE.MeshStandardMaterial({
 bulbLight.add(new THREE.Mesh(bulbGeometry, bulbMat));
 bulbLight.position.set(0, 2, 0);
 bulbLight.castShadow = true;
-
-const hemiLight = new THREE.HemisphereLight(0xddeeff, 0x0f0e0d, 0.02);
-
-scene.add(light);
-scene.add(hemiLight);
 scene.add(bulbLight);
 
-function animate() {
-  requestAnimationFrame(animate);
-  render();
-}
+const hemiLight = new THREE.HemisphereLight(0xddeeff, 0x0f0e0d, 0.02);
+scene.add(hemiLight);
 
 // 큐브 추가 및 움직임
 function render() {
@@ -80,6 +74,11 @@ function render() {
 
   // 파이널 랜더링
   renderer.render(scene, camera);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  render();
 }
 
 animate();
